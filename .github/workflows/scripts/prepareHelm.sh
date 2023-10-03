39cf218b56a8f6397e8b3dd0e13027e7cf7dfad2
Ryan Faircloth <ryan@dss-i.com>
2023-10-03 09:39:44 -0400

    fix: update ci for publish

--- .github/workflows/scripts/prepareHelm.sh
+++ .github/workflows/scripts/prepareHelm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #
-yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" chart/Chart.yaml
+yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" charts/s3proxy/Chart.yaml
 helm package chart
--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -3,15 +3,15 @@ set -ev
 helm push $(ls *.tgz) oci://ghcr.io/${GITHUB_REPOSITORY_OWNER}/charts
 
 unset GITHUB_TOKEN
-export GH_TOKEN=$SEGWAY_CHARTS_WRITE
+export GH_TOKEN=$CHARTS_WRITE
 
 gh auth setup-git
 pushd /tmp
-gh repo clone ${GITHUB_REPOSITORY_OWNER}/${SEGWAY_CHARTS_REPO}
+gh repo clone ${GITHUB_REPOSITORY_OWNER}/charts
 popd
-helm repo index . --url https://github.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY#*/}/releases/download/v$1 --merge /tmp/${SEGWAY_CHARTS_REPO}/index.yaml
-cp index.yaml /tmp/${SEGWAY_CHARTS_REPO}/index.yaml
-pushd /tmp/${SEGWAY_CHARTS_REPO}
+helm repo index . --url https://github.com/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY#*/}/releases/download/v$1 --merge /tmp/charts/index.yaml
+cp index.yaml /tmp/charts/index.yaml
+pushd /tmp/charts
 git add index.yaml
 git commit -am "chore(helm): Publish $1"
 git push
